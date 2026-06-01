edbeb9bb75030168d8f298c92a377d5052268453
Rae <544469923@qq.com>
2026-06-01 14:46:08 +0800

    Initial Commit

---
+++ JokeCommunityRp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokeCommunityRp
+//
+//  Created by  mac on 2026/6/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
